b47399d344e34d3b962cfad573b6089a5a6e5c8b
thes4s67 <thes4s67@gmail.com>
2022-09-09 22:48:50 -0700

    Update next config

--- app/next.config.js
+++ app/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    CLIENT_URL: process.env.CLIENT_URL,
+    SERVER_URL: process.env.SERVER_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
